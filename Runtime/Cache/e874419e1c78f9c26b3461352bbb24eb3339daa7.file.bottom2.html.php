bcb3ffa460b99fad92da03d3d6c28a0e15936238
leili <1473930135@qq.com>
2017-03-10 21:26:57 +0800

    雷力修改了
    2222

---
+++ Runtime/Cache/e874419e1c78f9c26b3461352bbb24eb3339daa7.file.bottom2.html.php
@@ -0,0 +1,40 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2017-02-28 10:54:40
+         compiled from "F:/zxyClothPat/Home/View\Common\bottom2.html" */ ?>
+<?php /*%%SmartyHeaderCode:635858b4e670658e31-63874592%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e874419e1c78f9c26b3461352bbb24eb3339daa7' => 
+    array (
+      0 => 'F:/zxyClothPat/Home/View\\Common\\bottom2.html',
+      1 => 1488090948,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '635858b4e670658e31-63874592',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_58b4e67067b71',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_58b4e67067b71')) {function content_58b4e67067b71($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+	<meta charset="utf-8">
+	<title>首页</title>
+	<style type="text/css">
+		.bottom{
+			height: 50px;
+			width: 100%;
+			margin-top:40px; 
+			border-top: 1px solid rgba(204, 204, 204, 0.7);
+			box-shadow: 2px 2px 15px rgba(204, 204, 204, 0.35);		
+		}
+	</style>
+</head>
+<body>
+	<div class="bottom">	
+	</div>
+</body>
+</html><?php }} ?>
