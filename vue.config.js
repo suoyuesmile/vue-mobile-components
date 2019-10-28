64446ddd0716ac9d58f95cbf7276c154426cffb2
shaosuo <suoyuesmile@163.com>
2019-10-28 16:10:01 +0800

    Add: vue.config

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,10 @@ module.exports = {
   filenameHashing: true,
   lintOnSave: true,
   runtimeCompiler: false,
-  transpileDependencies: [
-    /\/node_modules\/vue-echarts\//,
-    /\/node_modules\/resize-detector\//
-  ],
+  transpileDependencies: [/\/node_modules\/vue-echarts\//, /\/node_modules\/resize-detector\//],
   productionSourceMap: false,
   devServer: {
-    before: require('./mock'),
+    // before: require('./mock'),
     // 单元测试、本地联调情况下，使用代理灰度数据
     proxy: {
       '^/api': {
@@ -19,13 +16,13 @@ module.exports = {
       }
     }
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
-            .rule('pug')
-            .test(/\.pug$/)
-            .use('pug-html-loader')
-            .loader('pug-html-loader')
-            .end()
+      .rule('pug')
+      .test(/\.pug$/)
+      .use('pug-html-loader')
+      .loader('pug-html-loader')
+      .end()
   },
   css: {
     sourceMap: true
